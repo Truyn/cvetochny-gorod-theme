c82763f1a05f4928ccb36d35e8f7d40ed608b8d9
Truyn <53527324+Truyn@users.noreply.github.com>
2026-07-27 13:42:20 +0300

    Добавить широкую область персонализации под карточкой

--- single-product.php
+++ single-product.php
@@ -22,15 +22,21 @@ get_header();
                 <?php echo get_the_password_form(); ?>
             <?php else : ?>
                 <article id="product-<?php the_ID(); ?>" <?php wc_product_class('cg-product-detail', $product); ?>>
-                    <div class="cg-product-detail__top">
-                        <section class="cg-product-detail__gallery" aria-label="Фотографии товара">
-                            <?php do_action('woocommerce_before_single_product_summary'); ?>
-                        </section>
+                    <form class="cg-product-detail__form" method="post" enctype="multipart/form-data">
+                        <div class="cg-product-detail__top">
+                            <section class="cg-product-detail__gallery" aria-label="Фотографии товара">
+                                <?php do_action('woocommerce_before_single_product_summary'); ?>
+                            </section>
 
-                        <section class="cg-product-detail__summary summary entry-summary" aria-label="Информация о товаре">
-                            <?php do_action('woocommerce_single_product_summary'); ?>
-                        </section>
-                    </div>
+                            <section class="cg-product-detail__summary summary entry-summary" aria-label="Информация о товаре">
+                                <?php do_action('woocommerce_single_product_summary'); ?>
+                            </section>
+                        </div>
+
+                        <div class="cg-product-detail__options">
+                            <?php do_action('cg_product_options_area'); ?>
+                        </div>
+                    </form>
 
                     <div class="cg-product-detail__bottom">
                         <?php do_action('woocommerce_after_single_product_summary'); ?>
